f2c74c259b81cd6d0ef291e5f404da91d4402119
Hieu Nguyen <htn619@gmail.com>
2022-04-24 19:42:29 -0700

    got index page showing and added new link

---
+++ models/products.js
@@ -0,0 +1,22 @@
+//Dependencies
+
+const mongoose = require("./connection") //import the already connected object
+
+//Schemas & Models
+
+//schema the definition of our data type
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    img: String,
+    price: Number,
+    qty: Number
+}, {timestamps: true});
+
+//model the object for working with our data type
+const Products = mongoose.model("Products", productSchema)
+
+//export the model
+
+module.exports = Products
---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT;
+
+const methodOverride = require('method-override')
+const product = require("./models/seed.js")
+
+//middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride("_method"))
+
+
+//routes
+app.get("/",(req,res)=> {
+    res.render('index.ejs', {allProducts: product})
+})
+app.get("/:id", (req, res) => {
+    res.send('hello')
+})
+
+// listener
+app.listen(PORT, () => {
+    console.log(`On PORT ${PORT}`);
+});
