336752b7aa86b268a22542f1e12a59fc0502e6de
Hieu Nguyen <htn619@gmail.com>
2022-04-24 20:04:44 -0700

    index page is working with clickable links to the product

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override')
 const product = require("./models/seed.js")
 
 //middleware
+
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride("_method"))
 
@@ -18,9 +19,12 @@ app.get("/",(req,res)=> {
     res.render('index.ejs', {allProducts: product})
 })
 app.get("/:id", (req, res) => {
-    res.send('hello')
+    res.render('show.ejs', {
+        products: product[req.params.id],
+        index: req.params.id
+    })
 })
-
+   
 // listener
 app.listen(PORT, () => {
     console.log(`On PORT ${PORT}`);
